cf886687c9b9ccc5588f57a9d93f3560020068b6
MrMatiz2 <alejandromatiz87@gmail.com>
2024-04-02 07:35:44 -0500

    Init project

---
+++ src/main/java/com/example/interceptor/entity/Student.java
@@ -0,0 +1,14 @@
+package com.example.interceptor.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class Student {
+
+    private int id;
+    private String firstName;
+    private String lastName;
+
+}
